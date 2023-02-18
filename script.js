08ef3bf1028f9429cca9f3c7b018ae3bc82a7adb
AlessGarau <alessandrog.pro@gmail.com>
2023-02-19 00:38:59 +0100

    verify input

--- script.js
+++ script.js
@@ -6,3 +6,37 @@
 // - Any `input` field is empty. The message for this error should say *"[Field Name] cannot be empty"*
 // - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`).
 //  The message for this error should say *"Looks like this is not an email"*
+
+function getValue(input){
+    value = document.getElementById(input).value
+}
+
+// let FirstName = getValue("First Name")
+// let LastName = getValue("Last Name")
+// let EmailAddress = getValue("Email Address")
+// let Password = getValue("Password")
+
+function verifyEmpty(variable){
+    if(!variable){
+        console.log(variable)
+    } else{
+        alert(variable.name, "Champ vide")
+    }
+}
+let FirstName = document.getElementById("FirstName")
+let LastName = document.getElementById("LastName")
+let EmailAddress = document.getElementById("EmailAddress")
+let Password = document.getElementById("Password")
+
+let inputArray = [FirstName, LastName, EmailAddress, Password]
+
+let allInputs = document.querySelectorAll("input")
+
+let freeTrial = document.getElementById("FreeTrial")
+
+freeTrial.addEventListener('click', () => {
+
+    inputArray.forEach(input => 
+        verifyEmpty(input.value)
+        )
+})
