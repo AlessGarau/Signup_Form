4f96b8e13ca8d68552dc5ce4364b3c5719625176
AlessGarau <alessandrog.pro@gmail.com>
2023-02-27 11:47:24 +0100

    fix: isEmpty input

--- script.js
+++ script.js
@@ -7,39 +7,40 @@
 // - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`).
 //  The message for this error should say *"Looks like this is not an email"*
 
+function isEmpty(array) {
+  array.forEach(input => {
+    if (input.value === "") {
+      input.classList.add("wrongInput")
+      input.placeholder = `${input.name} cannot be empty`
+    } else {
+      input.classList.remove("wrongInput")
+      return true;
+    }
+  })
 
-function isEmpty() {
-  const firstNameInput = document.getElementById("FirstName");
-  const lastNameInput = document.getElementById("LastName");
-  const emailAddressInput = document.getElementById("EmailAddress");
-  const passwordInput = document.getElementById("Password");
-
-  if (firstNameInput.value === "" || lastNameInput.value === "" || emailAddressInput.value === "" || passwordInput.value === "") {
-    alert("Veuillez remplir tous les champs.");
-    return false;
-  }
-  else {
-    console.log('yessir')
-    return true;
-  }
 }
 
 function validateEmail(email) {
   // Expression régulière pour vérifier si l'email est valide
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-  return regex.test(email) //return true/false
+  return regex.test(email) //return true
 }
+
+const firstNameInput = document.getElementById("FirstName");
+const lastNameInput = document.getElementById("LastName");
+const emailAddressInput = document.getElementById("EmailAddress");
+const passwordInput = document.getElementById("Password");
+
+let inputArray = [firstNameInput, lastNameInput, lastNameInput, emailAddressInput, passwordInput]
 
 let freeTrial = document.getElementById('FreeTrial')
 
 freeTrial.addEventListener('click', () => {
   const emailAddress = document.getElementById("EmailAddress").value
-  isEmpty()
+  isEmpty(inputArray)
   validateEmail(emailAddress)
 })
 
-//changer remplir tous les champs et detecter quel champs ne sont pas remplit
-//check le format de l'email
 //faire ajustement css
 
 
